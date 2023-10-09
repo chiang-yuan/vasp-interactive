0d54ed2115e6adf385c67d477433086c0f9aebd8
Yuan Chiang <cyrusyc@lbl.gov>
2023-10-09 18:21:43 -0400

    successfully compile on Bridges2

---
+++ vasp-build/compile-bridges2.sh
@@ -0,0 +1,16 @@
+module purge
+
+module load intelmpi/20.4-intel20.4
+module load anaconda3
+source activate ms
+
+export NCORES=1
+
+TMP="/jet/home/ychiang4/tmp/"
+rm -rf $TMP
+mkdir $TMP
+
+ROOT=$TMP VASP_BINARY_PATH="/jet/home/ychiang4/.local/bin/" \
+INTERACTIVE_PATCH=patch.py \
+IPI_PATCH=patch_ipi.py \
+./compile_vasp.sh vasp.6.4.1.tgz makefile makefile.include
